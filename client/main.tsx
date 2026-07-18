3ea3062a726f52e6b711df4d8f8bbf02f47a8ba5
Balor44 <nwaforobinna77@gmail.com>
2026-07-18 12:22:47 -0700

    feat: Kasapp2 landing page built and working locally

--- client/main.tsx
+++ client/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import KasappLanding from './KasappLanding';
+import KasappLanding from './Kasapp2Landing';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
