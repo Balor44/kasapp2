88f6bd0e9d235c4403a3d5b424b12c958db742fe
Balor44 <nwaforobinna77@gmail.com>
2026-07-22 16:32:26 -0700

    Fix: Update server to reflect verify token

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import app from './app';
 import { connectDB } from './database/connect';
 
 const PORT = process.env.PORT || 3000;
+console.log('VERIFY TOKEN', process.env.WHATSAPP_VERIFY_TOKEN);
 
 const start = async () => {
   await connectDB();
