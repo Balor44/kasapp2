43671c2b7fcc272506fbfb4c8191ee0c7b8e4195
Balor44 <nwaforobinna77@gmail.com>
2026-07-31 17:14:10 -0700

    fix: successfully bundle tailwind styles and landing page updates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; 
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
@@ -7,7 +8,9 @@ export default defineConfig(({ mode }) => {
 
   return {
     root: 'client',
-    plugins: [react()],
+    plugins: [react(), 
+      tailwindcss()
+    ],
     build: {
       // Relative to root ('client'), '../dist-client' outputs to '/app/dist-client'
       outDir: '../dist-client',
