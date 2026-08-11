c067ca3b6997f3915ca8e5d5c40bc7e2baba520a
Balor44 <nwaforobinna77@gmail.com>
2026-08-11 03:17:38 -0700

    fix: remove hardcoded database name

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -8,8 +8,8 @@ export const connectDB = async (): Promise<void> => {
       throw new Error('MONGODB_URI environment variable is not set');
     }
 
-    await mongoose.connect(uri, { dbName: 'kasapp2' });
-    console.log('MongoDB connected to:', uri.split('@')[1], '- database: kasapp2');
+    await mongoose.connect(uri, { dbName: 'kasapp' });
+    console.log('MongoDB connected to:', uri.split('@')[1], '- database: kasapp');
   } catch (error) {
     console.error('MongoDB failed to connect:', error);
     console.log('Running without database - data will not persist');
